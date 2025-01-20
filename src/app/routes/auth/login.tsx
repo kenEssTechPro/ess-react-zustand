dbc9205e5ef58a0eb2276ee6d6381f59d574e4f0
kenEssTechPro <ken@ess-tech.pro>
2025-01-20 10:48:09 +0700

    feat: change login page

--- src/app/routes/auth/login.tsx
+++ src/app/routes/auth/login.tsx
@@ -1,15 +1,15 @@
-import { useNavigate, useSearchParams } from "react-router";
-import { AuthLayout } from "@/components/layouts/auth-layout";
-import { paths } from "@/config/paths";
-import { LoginForm } from "@/features/auth/components/login-form";
+import { useNavigate, useSearchParams } from 'react-router';
+import { AuthLayout } from '@/components/layouts/auth-layout';
+import { paths } from '@/config/paths';
+import { LoginForm } from '@/features/auth/components/login-form';
 
 const LoginRoute = () => {
   const navigate = useNavigate();
   const [searchParams] = useSearchParams();
-  const redirectTo = searchParams.get("redirectTo");
+  const redirectTo = searchParams.get('redirectTo');
 
   return (
-    <AuthLayout title="Log in to your account">
+    <AuthLayout title='Log in to your account Ken'>
       <LoginForm
         onSuccess={() => {
           navigate(`${redirectTo ? `${redirectTo}` : paths.users.getHref()}`, {
